7547d21212f4c9ca6df34a540a02b34bfe0fc056
Nebalzin <johnpechkin@gmail.com>
2024-05-02 17:48:22 +0300

    начинаем изучать JS

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const name_product = "Кроссовки NIKE";
+console.log('Наименование товара: ', name_product);
+const quantity_product = 20;
+// console.log('Количество товара: ', quantity_product);
+const category_product = "Обувь";
+// console.log('Категория товара: ', category_product);
+const price_product = 3999;
+// console.log('Цена товара: ', price_product);
+console.log("Общая сумма товара: ", quantity_product * price_product)
