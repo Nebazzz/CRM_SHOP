6136766032ee9d029041976460f1ab3161d0132d
Nebalzin <johnpechkin@gmail.com>
2024-05-03 21:50:47 +0300

    урок 2 готово

---
+++ einstein.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const myWeight = 70;
+const speedLight = 3e8;
+const qspeedLight = speedLight **2;
+const energy = myWeight * qspeedLight
+console.log("E = ", energy);
--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 'use strict';
 
-const name_product = "Кроссовки NIKE";
-console.log('Наименование товара: ', name_product);
-const quantity_product = 20;
-// console.log('Количество товара: ', quantity_product);
-const category_product = "Обувь";
-// console.log('Категория товара: ', category_product);
-const price_product = 3999;
-// console.log('Цена товара: ', price_product);
-console.log("Общая сумма товара: ", quantity_product * price_product)
+// 1 урок
+
+// const name_product = "Кроссовки NIKE";
+// // console.log('Наименование товара: ', name_product);
+// const quantity_product = 20;
+// // console.log('Количество товара: ', quantity_product);
+// const category_product = "Обувь";
+// // console.log('Категория товара: ', category_product);
+// const price_product = 3999;
+// // console.log('Цена товара: ', price_product);
+
+// 2 урок
+
+const answer_name = prompt('Наименование товара');
+const answer_quantity = +prompt('Количество товара');
+const answer_category = prompt('Категория товара');
+const answer_price = +prompt('Цена товара');
+const summary_product =  answer_quantity * answer_price;
+console.log(`На складе ${answer_quantity} единицы товара ${answer_name} на сумму ${summary_product} деревянных`);
