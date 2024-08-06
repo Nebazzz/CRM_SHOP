bc7b9c148400ae5735f5c9c11b8c06f7b7be7022
Nebazzz <johnpechkin@gmail.com>
2024-08-06 21:21:58 +0300

    l11,3 done

--- script/lesson07/game.js
+++ script/lesson07/game.js
@@ -34,15 +34,53 @@
 
 // ================ Lesson 11 ==================
 
-const getNumber = (arr) => {
-  const randomNumber = Math.floor(Math.random() * 11);
-  arr.push(randomNumber);
-  const summary = arr.reduce((sum, current) => sum + current, 0);
-  if (summary < 50) {
-    return getNumber(arr);
-  } else {
-    return arr;
-  }
+// const getNumber = (arr) => {
+//   const randomNumber = Math.floor(Math.random() * 11);
+//   arr.push(randomNumber);
+//   const summary = arr.reduce((sum, current) => sum + current, 0);
+//   if (summary < 50) {
+//     return getNumber(arr);
+//   } else {
+//     return arr;
+//   }
+// };
+
+// console.log(getNumber([]));
+
+const rectangle = {
+  width: 5,
+  height: 5,
+
+
+set width(value) {
+    if (typeof value === 'number') {
+      this.width = value;
+    } else {
+      console.log(`Ошибка - ${value} не число!`);
+    }
+  },
+
+  set height(value) {
+    if (typeof value === 'number') {
+      this.height = value;
+    } else {
+      console.log(`Ошибка - ${value} не число!`);
+    }
+  },
+
+  get perimeter() {
+    const perimeter = 2 * (this.width + this.height);
+    return perimeter + 'см.';
+  },
+
+  get area() {
+    const area = this.width * this.height;
+    return area + 'см.';
+  },
 };
 
-console.log(getNumber([]));
+rectangle.width = 2;
+rectangle.height = 3;
+
+console.log(rectangle.perimeter);
+console.log(rectangle.area);
