a23eaf85bfddf7e039b184a2019fd31d9d0fbf28
Nebazzz <johnpechkin@gmail.com>
2024-08-09 20:30:29 +0300

    eng lang done

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 'use strict';
 
 (() => {
+  const FIGURES_ENG = ['rock', 'scissors', 'paper'];
   const FIGURES_RUS = ['камень', 'ножницы', 'бумага'];
 
   const getRandomIntInclusive = (min, max) => {
@@ -9,7 +10,17 @@
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
 
-  const game = () => {
+  const getFigure = (lang) => {
+    if (lang === 'ENG' || lang === 'EN') {
+      return FIGURES_ENG;
+    } else {
+      return FIGURES_RUS;
+    }
+  };
+
+  const game = (language = '') => {
+    const arrFigure = getFigure(language);
+
     const result = {
       player: 0,
       computer: 0,
@@ -17,61 +28,86 @@
 
     const checkFigure = (figure, index = 0) => {
       figure = figure.toLowerCase();
-      if (index >= FIGURES_RUS.length) {
+      if (index >= arrFigure.length) {
         return -1;
       }
-      if (FIGURES_RUS[index].startsWith(figure)) {
+      if (arrFigure[index].startsWith(figure)) {
         return index;
       }
       return checkFigure(figure, index + 1);
     };
 
     const playGame = () => {
-      const playerInput = prompt('Камень, ножницы, бумага?', '').toLowerCase();
+      const promptMessage = (language === 'ENG' || language === 'EN') ?
+      'Rock, scissors, paper?' :
+      'Камень, ножницы, бумага?';
+      const invalidInputAlert = (language === 'ENG' || language === 'EN') ?
+      'Invalid input! Please choose rock, scissors, or paper.' :
+      'Неверный ввод. Пожалуйста, выберите камень, ножницы или бумага';
+      const playerInput = prompt(promptMessage, '').toLowerCase();
       const playerFigureIndex = checkFigure(playerInput);
 
       if (playerFigureIndex === -1) {
-        alert('Неверный ввод. Пожалуйста, выберите камень, ножницы или бумага.');
-        if (confirm('Попробовать снова?')) {
+        alert(invalidInputAlert);
+        if (confirm(language === 'ENG' || language === 'EN' ? 'Try again?' : 'Попробовать снова?')) {
           playGame();
         } else {
-          alert(`Игра завершена. Итоговые очки - Компьютер: ${result.computer}, Вы: ${result.player}`);
+          alert((language === 'ENG' || language === 'EN' ?
+            `Game over. Final score - Computer: ${result.computer}, You: ${result.player}` :
+            `Игра завершена. Итоговые очки - Компьютер: ${result.computer}, Вы: ${result.player}`));
         }
         return;
       }
 
       const computerFigureIndex = getRandomIntInclusive(0, 2);
-
-      const playerFigure = FIGURES_RUS[playerFigureIndex];
-      const computerFigure = FIGURES_RUS[computerFigureIndex];
+      const playerFigure = arrFigure[playerFigureIndex];
+      const computerFigure = arrFigure[computerFigureIndex];
 
       if (
-        (playerFigure === 'камень' && computerFigure === 'ножницы') ||
-        (playerFigure === 'ножницы' && computerFigure === 'бумага') ||
-        (playerFigure === 'бумага' && computerFigure === 'камень')
+        (playerFigure === arrFigure[0] && computerFigure === arrFigure[1]) ||
+        (playerFigure === arrFigure[1] && computerFigure === arrFigure[2]) ||
+        (playerFigure === arrFigure[2] && computerFigure === arrFigure[0])
       ) {
         result.player++;
-        alert(`Компьютер выбрал ${computerFigure}, вы выбрали ${playerFigure}. Вы победили!`);
+        alert((language === 'ENG' || language === 'EN' ?
+          `Computer chose ${computerFigure}, you chose ${playerFigure}. You win!` :
+          `Компьютер выбрал ${computerFigure}, вы выбрали ${playerFigure}. Вы победили!`));
       } else if (
-        (computerFigure === 'камень' && playerFigure === 'ножницы') ||
-        (computerFigure === 'ножницы' && playerFigure === 'бумага') ||
-        (computerFigure === 'бумага' && playerFigure === 'камень')
+        (computerFigure === arrFigure[0] && playerFigure === arrFigure[1]) ||
+        (computerFigure === arrFigure[1] && playerFigure === arrFigure[2]) ||
+        (computerFigure === arrFigure[2] && playerFigure === arrFigure[0])
       ) {
         result.computer++;
-        alert(`Компьютер выбрал ${computerFigure}, вы выбрали ${playerFigure}. Компьютер победил!`);
+        alert((language === 'ENG' || language === 'EN' ?
+          `Computer chose ${computerFigure}, you chose ${playerFigure}. Computer wins!` :
+          `Компьютер выбрал ${computerFigure}, вы выбрали ${playerFigure}. Компьютер победил!`));
       } else {
-        alert(`Компьютер выбрал ${computerFigure}, вы выбрали ${playerFigure}. Ничья!`);
+        alert((language === 'ENG' || language === 'EN' ? `Computer chose ${computerFigure}, you chose ${playerFigure}. It's a draw!` :
+          `Компьютер выбрал ${computerFigure}, вы выбрали ${playerFigure}. Ничья!`));
       }
 
-      if (confirm('Сыграть еще раз?')) {
+      if (confirm(language === 'ENG' || language === 'EN' ?
+        'Play again?' :
+        'Сыграть еще раз?')) {
+
         playGame();
+
       } else {
-        alert(`Игра завершена. Итоговые очки - Компьютер: ${result.computer}, Вы: ${result.player}`);
+        alert((language === 'ENG' || language === 'EN' ?
+          `Game over. Final score - Computer: ${result.computer}, You: ${result.player}` :
+          `Игра завершена. Итоговые очки - Компьютер: ${result.computer}, Вы: ${result.player}`));
       }
     };
 
     playGame();
   };
 
-  window.RPS = game;
+  const startGame = prompt('Выберите язык / choose the language: ENG / RU', '').toLowerCase();
+  if (startGame.startsWith('en')) {
+    game('ENG');
+  } else {
+    game();
+  }
+  // window.RPS = game;
+
 })();
