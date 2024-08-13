ec10fc829ad0e767a20440a1325ba809f2dfc9dd
Nebazzz <johnpechkin@gmail.com>
2024-08-13 12:39:42 +0300

    add computerTurn and function getnoun

--- marbles.js
+++ marbles.js
@@ -11,7 +11,22 @@
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
 
-  const playGame = () => {
+  const getNoun = (number, one, two, five) => {
+    let n = Math.abs(number) % 100;
+    if (n >= 5 && n <= 20) {
+      return five;
+    }
+    n %= 10;
+    if (n === 1) {
+      return one;
+    }
+    if (n >= 2 && n <= 4) {
+      return two;
+    }
+    return five;
+  };
+
+  const playerTurn = () => {
     const playerInput = prompt(`Шарики игрока: ${playerMarbles}. Введите, пожалуйста, количество шариков от 1 до ${playerMarbles}`, '');
     const playerGuess = parseInt(playerInput);
 
@@ -20,7 +35,7 @@
       playerGuess > playerMarbles
     ) {
       alert(`Неверный ввод! Введите количество шариков заново!`);
-      return true; // Продолжаем игру при неверном вводе
+      return playerTurn();
     }
 
     const computerGuess = getRandomIntInclusive(0, 1);
@@ -32,34 +47,66 @@
       (computerGuess === 1 && !isPlayerGuessEven)) {
       computerMarbles += playerGuess;
       playerMarbles -= playerGuess;
-      alert(`Компьютер угадал! У вас осталось: ${playerMarbles} шариков, у компьютера: ${computerMarbles}`);
+      alert(`Компьютер угадал! У вас: ${playerMarbles} ${getNoun(playerMarbles, 'шарик', 'шарика', 'шариков')}, у компьютера: ${computerMarbles} ${getNoun(computerMarbles, 'шарик', 'шарика', 'шариков')}`);
     } else {
       playerMarbles += playerGuess;
       computerMarbles -= playerGuess;
-      alert(`Компьютер не угадал! У вас: ${playerMarbles} шариков, у компьютера осталось: ${computerMarbles}`);
+      alert(`Компьютер не угадал! У вас: ${playerMarbles} ${getNoun(playerMarbles, 'шарик', 'шарика', 'шариков')}, у компьютера: ${computerMarbles} ${getNoun(computerMarbles, 'шарик', 'шарика', 'шариков')}`);
     }
 
     if (playerMarbles <= 0) {
       alert(`Вы проиграли! У вас закончились шарики!`);
-      return false;
+      return;
     }
 
     if (computerMarbles <= 0) {
       alert(`Вы выиграли! У компьютера закончились шарики!`);
-      return false;
+      return;
     }
 
-    return true;
+    computerTurn();
+  };
+
+  const computerTurn = () => {
+    const computerGuess = getRandomIntInclusive(1, computerMarbles);
+    const isComputerGuessEven = computerGuess % 2 === 0;
+
+    const playerGuess = confirm(`Компьютер выбрал количество шариков. Это количество чётное?`);
+
+    if ((playerGuess && isComputerGuessEven) ||
+      (!playerGuess && !isComputerGuessEven)) {
+      playerMarbles += computerGuess;
+      computerMarbles -= computerGuess;
+      alert(`Вы угадали! У вас: ${playerMarbles} ${getNoun(playerMarbles, 'шарик', 'шарика', 'шариков')}, у компьютера: ${computerMarbles} ${getNoun(computerMarbles, 'шарик', 'шарика', 'шариков')}`);
+    } else {
+      playerMarbles -= computerGuess;
+      computerMarbles += computerGuess;
+      alert(`Вы не угадали! У вас: ${playerMarbles} ${getNoun(playerMarbles, 'шарик', 'шарика', 'шариков')}, у компьютера : ${computerMarbles} ${getNoun(computerMarbles, 'шарик', 'шарика', 'шариков')}`);
+    }
+
+    if (playerMarbles <= 0) {
+      alert(`Вы проиграли! У вас закончились шарики!`);
+      return;
+    }
+
+    if (computerMarbles <= 0) {
+      alert(`Вы выиграли! У компьютера закончились шарики!`);
+      return;
+    }
+
+    playerTurn();
+  };
+
+  const playGame = () => {
+    playerTurn();
   };
 
   const startGame = () => {
     alert(`Игра "Марблы". У вас и компьютера по ${startMarbles} шариков.`);
+    playerMarbles = startMarbles;
+    computerMarbles = startMarbles;
 
-    let continueGame = true;
-
-    while (continueGame) {
-      continueGame = playGame();
-    }
+    playGame();
 
     const playAgain = confirm('Сыграть еще раз?');
     if (playAgain) {
