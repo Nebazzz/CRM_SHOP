e9d4e354e843631acfe3b4ec1d23f5d5bdc0231d
Nebazzz <johnpechkin@gmail.com>
2024-08-19 14:11:53 +0300

    add modal.js + overlay modificator to display modal

---
+++ script/modal.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const modalVisible = document.querySelector('.overlay-visible');
+const modalTitle = document.querySelector('.page__title-text');
+const modalIdName = document.querySelector('.page__title-button-text');
+const modalIdButton = document.querySelector('.page__title-button-img');
+const modalForm = document.querySelector('.form');
+const modalFormCheckbox= document.querySelector('.form__input-checkbox');
+const modalFormInput= document.querySelector('.form__input-number');
+const modalSum = document.querySelector('.summary-number');
